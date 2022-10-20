b6fd56cd27a9e8af737d3e382763ef8f2dae60cf
cosmiccapybara <37220709+cosmiccapybara@users.noreply.github.com>
2022-10-20 18:55:51 +1100

    Update scale_human_ATACseq_signal.R
    Scale human ovary and testis ATAC-seq signal

--- predicted_germline_RT/scale_human_ATACseq_signal.R
+++ predicted_germline_RT/scale_human_ATACseq_signal.R
@@ -9,6 +9,3 @@ ovary <- fread("ENCFF586QDS_ovary_hg38.bed.gz")
 ovary[,7] <- scale(x=ovary[,7], center=T, scale=T)
 write.table(x= testis[,c(1:3,7)], file = "ENCFF289LWU_ATAC_testis_hg38_scaled.bed", col.names=F, row.names=F, sep="\t", quote=F)
 write.table(x= ovary[,c(1:3,7)], file = "ENCFF586QDS_ATAC_ovary_hg38_scaled.bed", col.names=F, row.names=F, sep="\t", quote=F)
-
-
-
