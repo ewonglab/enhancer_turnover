3c23c32f72c81012ff5a454a330ddaf9d4b91cec
cosmiccapybara <37220709+cosmiccapybara@users.noreply.github.com>
2022-10-20 18:54:46 +1100

    Update human_ovary_ATACsignal.R
    Scale human ovary and testis ATAC-seq signal

--- predicted_germline_RT/human_ovary_ATACsignal.R
+++ predicted_germline_RT/human_ovary_ATACsignal.R
@@ -1,17 +1,8 @@
-Replicon for replication time simulation - from human testis and ovary ATAC-seq
-
-Mouse test
-
-
-cd /g/data/zk16/cc3704/replication_timing
-export PATH=/g/data/zk16/xzhang/bin:$PATH
-mouse_path="/g/data/zk16/cc3704/replication_timing/mouse/testis_DNAseq_mm10_norm/chr1/"
-human_path="/g/data/zk16/cc3704/replication_timing/human/ENCFF289LWU_ATAC_testis_hg19_IPLS/chr1/"
-replicon -threads 8 ${mouse_path}chr1_ENCFF728ZKQ_testis_DNAseq_mm10_small_norm chr1_mm10_norm
-
-Human
 
 library(data.table)
+
+#read and scale ATAC-seq signal
+
 testis <- fread("ENCFF289LWU_ATAC_testis_hg38.bed")
 testis[,7] <- scale(x=testis[,7], center=T, scale=T)
 ovary <- fread("ENCFF586QDS_ovary_hg38.bed.gz")
